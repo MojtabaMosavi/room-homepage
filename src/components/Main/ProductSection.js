983ee912f42283e218e5fc23e5b413691bb6e512
Mojtaba Mosavi <Mosevi.m@yahoo.com>
2022-02-15 13:42:58 +0100

    Replaced ArticleData to the component itself

--- src/components/Main/ProductSection.js
+++ src/components/Main/ProductSection.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Article from "./Article";
-const articleData = require("./ArticleData.js");
+
+
+const articleData =  [
+    {
+        heading: " About our furniture",
+        text: "Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space."
+    }
+]
 
 
 import productImageDark from "../../assets/images/image-about-dark.jpg"
